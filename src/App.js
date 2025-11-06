9b103d17cbda475e46d6287a356cc90438f29cf4
Полина Малышева <malysheva.pm@phystech.edu>
2025-11-06 23:15:37 +0300

    add card component with like functionality

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
+import data from './assets/mock-data.json'
 
-function App() {
+const App = () => {
 
-const data = [
-  {
-    "title": "Tesla",
-    "text": "Best company",
-    "currentLikes": 7
-  },
-  {
-    "title": "VK",
-    "text": "Messenger for children",
-    "currentLikes": 2
-  },
-  {
-    "title": "Me",
-    "text": "Best student",
-    "currentLikes": 999
-  },
-  {
-    "title": "Item",
-    "text": "Some other item",
-    "currentLikes": 0
-  },
-  {
-    "title": "Real article",
-    "text": "Real text for real article",
-    "currentLikes": 11
-  }
-]
-
-
-const Cards = data.map( d => <Card title={d.title} text={d.text} currentLikes= {d.currentLikes}/>)
+  let Cards = data.map( d => <Card card={d}/>)
 
   return (
     <div className="App">
       <Header/>
-      <div className='Cards_container'>
+      <div className='Cards-container'>
       {Cards}
       </div>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import { useState } from 'react';
 import './Card.css';
+import like from './../assets/images/like.png'
+import antiLike from './../assets/images/antiLike.png'
 
 const Card = (props) => {
-    return(
+
+    const [likes, setLikes] = useState(props.card.currentLikes);
+    const [isLiked, setIsLiked] = useState(false);
+
+    const click = () => {
+        if (isLiked) {
+            setLikes(likes - 1);
+        } else {
+            setLikes(likes + 1);
+        }
+        setIsLiked(!isLiked);
+    };
+
+    return (
         <div className='Card'>
             <div className='Title'>
-                {props.title}
+                {props.card.title}
             </div>
             <div className='Text'>
-                {props.text}
+                {props.card.text}
             </div>
-            <div className = 'Likes'>
-                <div>{props.currentLikes}</div>
-                <button>лайк</button>
+            <div className='Likes'>
+                <div>{likes}</div>
+                <button onClick={click}>
+                    <img
+                        src={isLiked ? like : antiLike}
+                        width="24"
+                        height="24" />
+                </button>
             </div>
         </div>
     )
